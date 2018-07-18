78817a59c70e78844ebc39d087f7a0bd3a66ae18
poptart-sommelier <rich.d.nospam@gmail.com>
2018-07-17 22:05:54 -0400

    first commit, on chapter 5

---
+++ README.md
@@ -0,0 +1,10 @@
+GUIDE:
+https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-ii-templates
+
+launching:
+# launch the virtual env:
+'. venv/bin/activate'
+# export the flask app variable:
+export FLASK_APP=twitter_web_app.py
+# run the app
+flask run
---
+++ app/models.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class Users(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), index=True, unique=True)
+	email = db.Column(db.String(120), index=True, unique=True)
+	password_hash = db.Column(db.String(128))
+	posts = db.relationship('Posts', backref='author', lazy='dynamic')
+
+	def __repr__(self):
+		return '<Users {}>'.format(self.username)
+
+	def set_password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def check_password(self, password):
+		return check_password_hash(self.password_hash, password)
+
+class Posts(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	body = db.Column(db.String(140))
+	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+	def __repr__(self):
+		return '<Posts {}>'.format(self.body)
